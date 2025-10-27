22c860d7e65142b7cc862d07c4fb268638f08d5f
mag-galaxy <kaiching.yang.margar@gmail.com>
2025-10-27 20:41:54 +0800

    update predict functions

--- train_model.py
+++ train_model.py
@@ -1,47 +1,42 @@
 import pandas as pd
-import sklearn
-import argparse
-import joblib
-import os
 
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-
-# ================================= arguments =================================
-parser = argparse.ArgumentParser(description="TTMATCH model training")
-parser.add_argument('-f', '--datafile', default="train.csv", help="training data file name")
-parser.add_argument('-f', '--testfile', default="test.csv", help="testing data file name")
-args = parser.parse_args()
-DATA_FILE = args.datafile
-TEST_FILE = args.testfile
+from xgboost import XGBClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, classification_report
 
 # ================================= parameters =================================
+DATA_FILE = "train.csv"
+TEST_FILE = "test.csv"
 RESULT_FILE = "111504504_subission.csv"
 
 # ================================= functions =================================
-def predict_serverGetPoint(training_data, testing_data, submission):
-    # select features
-    training_data = training_data.drop(columns=["label"])
-
+def predict_serverGetPoint(training_data, testing_data):
     # separate features and targets
     X = training_data.drop(columns=["serverGetPoint"])
     y = training_data["serverGetPoint"]
     le = LabelEncoder()
     y_encoded = le.fit_transform(y)
-    # X_train, X_test, y_train, y_test = train_test_split(X, y_encoded, test_size=0.3, random_state=42, stratify=y)
-    X_train = X
-    y_train = y
+    X_train, X_val, y_train, y_val = train_test_split(X, y_encoded, test_size=0.2, random_state=42, stratify=y_encoded)
 
+    # train
     model = RandomForestClassifier(n_estimators=100, random_state=42)
+    # model = XGBClassifier(use_label_encoder=False, eval_metric='mlogloss')
+    # model = LogisticRegression(max_iter=1000)
     model.fit(X_train, y_train)
 
-    X_test = testing_data
+    # validate
+    y_val_pred = model.fit(X_val, y_val)
+
+    # predict testing set
+    X_test = testing_data[X.columns]
     y_pred = model.predict(X_test)
 
-    submission["serverGetPoint"] = y_pred
+    print(classification_report(y_test, y_pred, target_names=le.classes_))
 
-    return
+    return y_pred
 
 def predict_pointId(data):
     return
@@ -56,3 +51,4 @@ df_data = pd.read_csv(DATA_FILE)
 df_test = pd.read_csv(TEST_FILE)
 df_result = pd.read_csv(RESULT_FILE)
 
+df_result["serverGetPoint"] = predict_serverGetPoint(df_data, df_test)
