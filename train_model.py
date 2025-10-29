35e7673f38963f70aea52a195146ba8e7b44eb4d
mag-galaxy <kaiching.yang.margar@gmail.com>
2025-10-29 15:17:48 +0800

    update predict function

--- train_model.py
+++ train_model.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy
 
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
@@ -10,16 +11,18 @@ from sklearn.metrics import accuracy_score, classification_report
 # ================================= parameters =================================
 DATA_FILE = "train.csv"
 TEST_FILE = "test.csv"
-RESULT_FILE = "111504504_subission.csv"
+RESULT_FILE = "111504504_submission.csv"
 
 # ================================= functions =================================
-def predict_serverGetPoint(training_data, testing_data):
+def TT_predict(training_data, testing_data):
+    training_data = training_data.sort_values(["rally_uid", "strokeNum"]).reset_index(drop=True)
+
     # separate features and targets
     X = training_data.drop(columns=["serverGetPoint"])
     y = training_data["serverGetPoint"]
-    le = LabelEncoder()
-    y_encoded = le.fit_transform(y)
-    X_train, X_val, y_train, y_val = train_test_split(X, y_encoded, test_size=0.2, random_state=42, stratify=y_encoded)
+    # le = LabelEncoder()
+    # y_encoded = le.fit_transform(y)
+    X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
 
     # train
     model = RandomForestClassifier(n_estimators=100, random_state=42)
@@ -28,27 +31,36 @@ def predict_serverGetPoint(training_data, testing_data):
     model.fit(X_train, y_train)
 
     # validate
-    y_val_pred = model.fit(X_val, y_val)
+    y_val_pred = model.predict(X_val)
 
     # predict testing set
     X_test = testing_data[X.columns]
     y_pred = model.predict(X_test)
 
-    print(classification_report(y_test, y_pred, target_names=le.classes_))
+    # print(classification_report(y_val, y_val_pred))
 
     return y_pred
 
-def predict_pointId(data):
-    return
-
-def predict_actionId(data):
-    return
-    
-
 # ================================= main =================================
 # read data
-df_data = pd.read_csv(DATA_FILE)
+df_train = pd.read_csv(DATA_FILE)
 df_test = pd.read_csv(TEST_FILE)
 df_result = pd.read_csv(RESULT_FILE)
 
-df_result["serverGetPoint"] = predict_serverGetPoint(df_data, df_test)
+# check data shape
+print("train.csv shape: ", df_train.shape)
+print("train.csv headers: ", df_train.head())
+print("test.csv shape: ", df_test.shape)
+print("test.csv headers: ", df_test.head())
+print("submission.csv shape: ", df_result.shape)
+print("submission.csv headers: ", df_result.head())
+
+# call functions for training and predict
+print("start processing...")
+
+df_result["serverGetPoint"] = TT_predict(df_train, df_test)
+# df_result["pointId"] = predict_serverGetPoint(df_train, df_test)
+# df_result["actionId"] = predict_serverGetPoint(df_train, df_test)
+
+# save result (prediction)
+df_result.to_csv(RESULT_FILE)
