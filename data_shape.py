e8121f85319d4a254def8b14961b061fcdd886a2
mag-galaxy <kaiching.yang.margar@gmail.com>
2025-11-01 21:21:13 +0800

    update data shape checking functions

--- data_shape.py
+++ data_shape.py
@@ -1,5 +1,13 @@
 import pandas as pd
 
+def count(df, column):
+    cnt = 0
+    for i in range(len(df[column])):
+        if df[column][i] == 1:
+            cnt += 1
+    return cnt
+
 FILE_NAME = "test.csv"
 test = pd.read_csv(FILE_NAME)
 print("Max strokes in a rally:", test["strickNumber"].max())
+print("count of strikNumber == 1: ", count(test, "strickNumber"))
