917d192dceeb64924f596da7291e15a094d0f0ad
mag-galaxy <kaiching.yang.margar@gmail.com>
2025-11-03 13:57:22 +0800

    update less important codes

--- data_shape.py
+++ data_shape.py
@@ -11,3 +11,14 @@ FILE_NAME = "test.csv"
 test = pd.read_csv(FILE_NAME)
 print("Max strokes in a rally:", test["strickNumber"].max())
 print("count of strikNumber == 1: ", count(test, "strickNumber"))
+
+mod = pd.read_csv("111504504_submission.csv")
+len = len(mod["rally_uid"])
+print(len)
+
+for i in range(len):
+    if mod.loc[i, "actionId"] == -1 or mod.loc[i, "pointId"] == -1:
+        mod.loc[i, "actionId"] = -1
+        mod.loc[i, "pointId"] = -1
+
+mod.to_csv("111504504_submission.csv", index=False)
