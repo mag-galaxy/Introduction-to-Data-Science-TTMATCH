1ba41986734ee4b3352cf57032d64223d809a5ed
mag-galaxy <kaiching.yang.margar@gmail.com>
2025-11-14 01:48:59 +0800

    separate data preprocessind file and training file

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,66 +1,63 @@
+# ================================= libraries =================================
 import numpy as np
 import pandas as pd
+import os
+import argparse
 
-# from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.preprocessing.sequence import pad_sequences
 
+# ================================= arguments =================================
+parser = argparse.ArgumentParser(description="all in one")
+parser.add_argument('-l', '--sequence_len', default=8, help="sequence len")
+args = parser.parse_args()
+
+# ================================= parameters =================================
 TRAIN_FILE = "train.csv"
-TARGET = ["serverGetPoint", "actionId", "pointId"]
-FEAT_NOT_USED = ["rally_uid", "sex", "match", "numberGame", "rally_id", "strickNumber", "let"]
 FEATURES = [
     "serveId", "serveNumber", "strickId", "handId", 
     "strengthId", "spinId", "pointId", "actionId", "positionId"
 ]
-MAX_SEQ_LEN = 6
+MAX_SEQ_LEN = args.sequence_len
+FOLDER_NAME = f"data_len_{MAX_SEQ_LEN}"
 
-# def make_sequences(df):
-#     X, y_server, y_action, y_point = [], [], [], []
-#     for rally_uid, group in df.groupby("rally_uid"):
-#         # seq is features of each strick in an unique rally
-#         seq = group[FEATURES].values.tolist()
-#         strick_count = len(group)
+# ================================= functions =================================
+def make_sequences(df):
+    X, y_server, y_action, y_point = [], [], [], []
+    for rally_uid, group in df.groupby("rally_uid"):
+        # seq is features of each strick in an unique rally
+        seq = group[FEATURES].values.tolist()
+        strick_count = len(group)
         
-#         # 0~(k-1)th -> k-th
-#         for window_size in range(1, min(strick_count, MAX_SEQ_LEN + 1)):
-#             for k in range(0, strick_count - window_size):
-#                 hist_seq = seq[k:k+window_size]
-#                 hist_seq = pad_sequences([hist_seq[-MAX_SEQ_LEN:]], maxlen=MAX_SEQ_LEN, 
-#                                         padding='pre', truncating='pre', value=0)[0]
-#                 # add features (X) and 3 targets (y)
-#                 print(f"rally_uid = {rally_uid}, strickCount = {strick_count}, window_size = {window_size}, k = {k},\nhist_seq =\n{hist_seq}")
-#                 print("y_server", group.iloc[k+window_size]["serverGetPoint"])
-#                 print("y_action", group.iloc[k+window_size]["actionId"])
-#                 print("y_point", group.iloc[k+window_size]["pointId"])
-#                 X.append(hist_seq)
-#                 y_server.append(group.iloc[k+window_size]["serverGetPoint"])
-#                 y_action.append(group.iloc[k+window_size]["actionId"])
-#                 y_point.append(group.iloc[k+window_size]["pointId"])
+        for window_size in range(1, min(strick_count, MAX_SEQ_LEN + 1)):
+            for k in range(0, strick_count - window_size):
+                hist_seq = seq[k:k+window_size]
+                hist_seq = pad_sequences([hist_seq[-MAX_SEQ_LEN:]], maxlen=MAX_SEQ_LEN, 
+                                        padding='pre', truncating='pre', value=0)[0]
+                # add features (X) and 3 targets (y)
+                X.append(hist_seq)
+                y_server.append(group.iloc[k+window_size]["serverGetPoint"])
+                y_action.append(group.iloc[k+window_size]["actionId"])
+                y_point.append(group.iloc[k+window_size]["pointId"])
                  
-#     return np.array(X), np.array(y_server), np.array(y_action), np.array(y_point)
-    
-# main
-print("read data...")
-# df_train = pd.read_csv(TRAIN_FILE)
+    return np.array(X), np.array(y_server), np.array(y_action), np.array(y_point)
 
-# X_train, y_server, y_action, y_point = make_sequences(df_train)
+def data_preprocessing(file_path):
+    df_train = pd.read_csv(file_path)
 
-# np.save("X.npy", X_train)
-# np.save("y_server.npy", y_server)
-# np.save("y_action.npy", y_action)
-# np.save("y_point.npy", y_point)
+    # modify classes encoding
+    df_train["actionId"] = df_train["actionId"].replace(-1, 19)
+    df_train["pointId"] = df_train["pointId"].replace(-1, 10)
 
-# print("len of sequence data: ",len(X_train))
-# print("✅ preprocessed data saved")
+    # group data with rally_uid and save sequences
+    X_train, y_server, y_action, y_point = make_sequences(df_train)
+    np.save(f"{FOLDER_NAME}/X.npy", X_train)
+    np.save(f"{FOLDER_NAME}/y_server.npy", y_server)
+    np.save(f"{FOLDER_NAME}/y_action.npy", y_action)
+    np.save(f"{FOLDER_NAME}/y_point.npy", y_point)
+    print("✅ preprocessed data saved")
 
-df_tmp = pd.read_csv(TRAIN_FILE)
-print(FEATURES)
-vocab_sizes = [df_tmp[f].max() for f in FEATURES]  # 各特徵最大值+1
-print(vocab_sizes)
-vocab_sizes = [len(df_tmp[f].unique()) for f in FEATURES]
-print(vocab_sizes, "\n\n")
+    return
 
-df_tmp = pd.read_csv("test.csv")
-print(FEATURES)
-vocab_sizes = [df_tmp[f].max() for f in FEATURES]  # 各特徵最大值+1
-print(vocab_sizes)
-vocab_sizes = [len(df_tmp[f].unique()) for f in FEATURES]
-print(vocab_sizes)
+# ================================= main =================================
+os.makedirs(FOLDER_NAME, exist_ok=True)
+data_preprocessing(TRAIN_FILE)
--- train_model.py
+++ train_model.py
@@ -1,6 +1,7 @@
 # ================================= libraries =================================
 import numpy as np
 import pandas as pd
+import argparse
 
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.models import Sequential, Model
@@ -10,67 +11,25 @@ from sklearn.model_selection import train_test_split
 from tensorflow.keras.layers import Input, Embedding, Concatenate
 from tensorflow.keras.layers import TimeDistributed
 
+# ================================= arguments =================================
+parser = argparse.ArgumentParser(description="all in one")
+parser.add_argument('-l', '--sequence_len', default=8, help="sequence len")
+args = parser.parse_args()
+
 # ================================= parameters =================================
-TRAIN_FILE = "train.csv"
 TEST_FILE = "test.csv"
 RESULT_FILE = "111504504_submission.csv"
-TARGET = ["serverGetPoint", "actionId", "pointId"]
-FEAT_NOT_USED = ["rally_uid", "sex", "match", "numberGame", "rally_id", "strickNumber", "let"]
 FEATURES = [
     "serveId", "serveNumber", "strickId", "handId", 
     "strengthId", "spinId", "pointId", "actionId", "positionId"
 ]
-MAX_SEQ_LEN = 8
-EPOCHS = 35
+MAX_SEQ_LEN = args.sequence_len
+FOLDER_NAME = f"data_len_{MAX_SEQ_LEN}"
+EPOCHS = 40
 EMBED_DIM = 16
 VOCAB_SIZE = [3, 5, 5, 4, 5, 7, 11, 20, 5]
 
-df_train = pd.read_csv(TRAIN_FILE)
-
 # ================================= functions =================================
-def make_sequences(df):
-    X, y_server, y_action, y_point = [], [], [], []
-    for rally_uid, group in df.groupby("rally_uid"):
-        # seq is features of each strick in an unique rally
-        seq = group[FEATURES].values.tolist()
-        strick_count = len(group)
-        
-        for window_size in range(1, min(strick_count, MAX_SEQ_LEN + 1)):
-            for k in range(0, strick_count - window_size):
-                hist_seq = seq[k:k+window_size]
-                hist_seq = pad_sequences([hist_seq[-MAX_SEQ_LEN:]], maxlen=MAX_SEQ_LEN, 
-                                        padding='pre', truncating='pre', value=0)[0]
-                # add features (X) and 3 targets (y)
-                X.append(hist_seq)
-                y_server.append(group.iloc[k+window_size]["serverGetPoint"])
-                y_action.append(group.iloc[k+window_size]["actionId"])
-                y_point.append(group.iloc[k+window_size]["pointId"])
-                 
-    return np.array(X), np.array(y_server), np.array(y_action), np.array(y_point)
-
-def data_preprocessing(read_data):
-    if read_data:
-        # modify classes encoding
-        df_train["actionId"] = df_train["actionId"].replace(-1, 19)
-        df_train["pointId"] = df_train["pointId"].replace(-1, 10)
-
-        # group data with rally_uid and save sequences
-        X_train, y_server, y_action, y_point = make_sequences(df_train)
-        np.save("X.npy", X_train)
-        np.save("y_server.npy", y_server)
-        np.save("y_action.npy", y_action)
-        np.save("y_point.npy", y_point)
-        print("✅ preprocessed data saved")
-
-        return X_train, y_server, y_action, y_point
-    else:
-        # load exsiting data
-        X_train = np.load("data_len_8/X.npy")
-        y_server = np.load("data_len_8/y_server.npy")
-        y_action = np.load("data_len_8/y_action.npy")
-        y_point = np.load("data_len_8/y_point.npy")
-        return X_train, y_server, y_action, y_point
-
 def build_multi_embedding_lstm(vocab_sizes, num_server_classes, num_action_classes, num_point_classes, embedding_dim=16):
     inputs, embedded_features = [], []
 
@@ -109,23 +68,22 @@ def build_multi_embedding_lstm(vocab_sizes, num_server_classes, num_action_class
     return model
 
 # ================================= main =================================
-read_data = input("read train.csv and do data preprocessing?")
-X_train, y_server, y_action, y_point = data_preprocessing(int(read_data))
+# load preprocessed data
+print("load preprocessed data...")
+X_train = np.load(f"{FOLDER_NAME}/X.npy")
+y_server = np.load(f"{FOLDER_NAME}/y_server.npy")
+y_action = np.load(f"{FOLDER_NAME}/y_action.npy")
+y_point = np.load(f"{FOLDER_NAME}/y_point.npy")
 
-# load test.csv
-df_test = pd.read_csv(TEST_FILE)
-df_test["actionId"] = df_test["actionId"].replace(-1, 19)
-df_test["pointId"] = df_test["pointId"].replace(-1, 10)
-
-# check data length
 num_server_classes = len(np.unique(y_server))
 num_action_classes = len(np.unique(y_action))
 num_point_classes  = len(np.unique(y_point))
 
-print("category count of server: ",num_server_classes)
-print("category count of action: ", num_action_classes)
-print("category count of point: ", num_point_classes)
-print("length of training sequence data: ", len(X_train))
+# load test.csv
+print("load test.csv")
+df_test = pd.read_csv(TEST_FILE)
+df_test["actionId"] = df_test["actionId"].replace(-1, 19)
+df_test["pointId"] = df_test["pointId"].replace(-1, 10)
 
 # separate data
 def split_features(X): return [X[:, :, i] for i in range(X.shape[2])]
